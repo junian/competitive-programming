9d1b8ce77c52743a573a0df663f6bc68c85f0448
Junian Triajianto <author@junian.dev>
2024-12-06 19:46:02 +0700

    Add 2024 day 01

---
+++ docs/README.md
@@ -0,0 +1,12 @@
+# Advent of Code Solution
+
+This repo contains my personal solutions to the [Advent of Code](https://adventofcode.com) puzzles.
+
+## How to Run
+
+For example, if you want to run the 2024 puzzle for day 1, you can run the following command:
+
+```csharp
+cd src/2024/Day01
+dotnet run < input.txt
+```
---
+++ src/2024/Day01/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+var listA = new List<int> { };
+var listB = new List<int> { };
+var sum = 0;
+
+string? line;
+while ((line = Console.ReadLine()) != null)
+{
+    var nums = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    listA.Add(int.Parse(nums[0]));
+    listB.Add(int.Parse(nums[1]));
+}
+
+listA.Sort();
+listB.Sort();
+
+for(var i=0; i<listA.Count; i++)
+{
+    sum += Math.Abs(listA[i] - listB[i]);
+}
+
+Console.WriteLine(sum);
