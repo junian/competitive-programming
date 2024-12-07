398e7b9a65f90cacf71447cde0bada72f4b2695b
Junian Triajianto <author@junian.dev>
2024-12-07 11:30:07 +0700

    Add part02

--- src/2024/Day02/Program.cs
+++ src/2024/Day02/Program.cs
@@ -59,6 +59,75 @@ static void Part01(IList<string> inputList)
     Console.WriteLine(count);
 }
 
+static void Part02(IList<string> inputList)
+{
+    var count = 0;
+    foreach (var line in inputList)
+    {
+        var originalNums = line
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToList();
+
+        var numList = new List<List<int>>
+        {
+            originalNums
+        };
+
+        for(var i=0; i<originalNums.Count; i++)
+        {
+            var newNums = new List<int>();
+            newNums.AddRange(originalNums);
+            newNums.RemoveAt(i);
+            numList.Add(newNums);
+        }
+
+        foreach(var nums in numList)
+        {
+            var isValid = true;
+            var increaseOrDecrease = 0;
+
+            for(var i=1; i<nums.Count; i++)
+            {
+                if(nums[i] == nums[i-1])
+                {
+                    isValid = false;
+                    break;
+                }
+
+                if(increaseOrDecrease == 0)
+                {
+                    increaseOrDecrease = nums[i] - nums[i-1];
+                    increaseOrDecrease /= Math.Abs(increaseOrDecrease);
+                }
+
+                var diff = (nums[i] - nums[i-1]) * increaseOrDecrease;
+                if(diff < 0 )
+                {
+                    isValid = false;
+                    break;
+                }
+
+                var absDiff = Math.Abs(diff);
+                if(absDiff < 1 || absDiff > 3)
+                {
+                    isValid = false;
+                    break;
+                }
+            }
+
+            if(isValid)
+            {
+                count++;
+                break;
+            }
+        }
+    }
+
+    Console.WriteLine(count);
+}
+
 var inputList = GetInputList();
 
 Part01(inputList);
+Part02(inputList);
