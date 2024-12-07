dd78b933e3f6e402768066baed90f8d0d32d951d
Junian Triajianto <author@junian.dev>
2024-12-07 10:24:47 +0700

    Add day 03

--- src/2024/Day03/Program.cs
+++ src/2024/Day03/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.RegularExpressions;
+
+var total = 0;
+var pattern = @"mul\((\d+),(\d+)\)";
+var regex = new Regex(pattern);
+string? line;
+while ((line = Console.ReadLine()) != null)
+{
+    var matches = regex.Matches(line);
+    foreach (Match match in matches)
+    {
+        if (int.TryParse(match.Groups[1].Value, out int number1) && 
+            int.TryParse(match.Groups[2].Value, out int number2))
+        {
+            total += (number1 * number2);
+        }
+    }
+}
+
+Console.WriteLine(total);
