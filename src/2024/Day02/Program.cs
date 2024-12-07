39608c54bfb7b5b848e29d97ba463e4748daad36
Junian Triajianto <author@junian.dev>
2024-12-07 11:15:21 +0700

    Update

--- src/2024/Day02/Program.cs
+++ src/2024/Day02/Program.cs
@@ -1,47 +1,64 @@
-﻿var count = 0;
-
-string? line;
-while ((line = Console.ReadLine()) != null)
+﻿static IList<string> GetInputList()
 {
-    var nums = line
-        .Split(' ', StringSplitOptions.RemoveEmptyEntries)
-        .Select(x => int.Parse(x))
-        .ToList();
-
-    var isValid = true;
-    var increaseOrDecrease = 0;
-
-    for(var i=1; i<nums.Count; i++)
+    var list = new List<string> { };
+    var line = default(string?);
+    while ((line = Console.ReadLine()) != null)
     {
-        if(nums[i] == nums[i-1])
-        {
-            isValid = false;
-            break;
-        }
-
-        if(increaseOrDecrease == 0)
-        {
-            increaseOrDecrease = nums[i] - nums[i-1];
-            increaseOrDecrease /= Math.Abs(increaseOrDecrease);
-        }
-
-        var diff = (nums[i] - nums[i-1]) * increaseOrDecrease;
-        if(diff < 0 )
-        {
-            isValid = false;
-            break;
-        }
-
-        var absDiff = Math.Abs(diff);
-        if(absDiff < 1 || absDiff > 3)
-        {
-            isValid = false;
-            break;
-        }
+        list.Add(line);
     }
 
-    if(isValid)
-        count++;
+    return list;
 }
 
-Console.WriteLine(count);
+static void Part01(IList<string> inputList)
+{
+    var count = 0;
+    foreach (var line in inputList)
+    {
+        var nums = line
+            .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+            .Select(x => int.Parse(x))
+            .ToList();
+
+        var isValid = true;
+        var increaseOrDecrease = 0;
+
+        for(var i=1; i<nums.Count; i++)
+        {
+            if(nums[i] == nums[i-1])
+            {
+                isValid = false;
+                break;
+            }
+
+            if(increaseOrDecrease == 0)
+            {
+                increaseOrDecrease = nums[i] - nums[i-1];
+                increaseOrDecrease /= Math.Abs(increaseOrDecrease);
+            }
+
+            var diff = (nums[i] - nums[i-1]) * increaseOrDecrease;
+            if(diff < 0 )
+            {
+                isValid = false;
+                break;
+            }
+
+            var absDiff = Math.Abs(diff);
+            if(absDiff < 1 || absDiff > 3)
+            {
+                isValid = false;
+                break;
+            }
+        }
+
+        if(isValid)
+            count++;
+    }
+
+    Console.WriteLine(count);
+}
+
+var inputList = GetInputList();
+
+Part01(inputList);
