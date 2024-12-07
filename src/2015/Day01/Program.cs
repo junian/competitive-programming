7667859e815e30eb050b4db484b38f1d54a72373
Junian Triajianto <author@junian.dev>
2024-12-07 15:20:49 +0700

    part 01 done

---
+++ src/2015/Day01/Program.cs
@@ -0,0 +1,31 @@
+﻿static IList<string> GetInputList()
+{
+    var list = new List<string> { };
+    var line = default(string?);
+    while ((line = Console.ReadLine()) != null)
+    {
+        list.Add(line);
+    }
+
+    return list;
+}
+
+static void Part01(IList<string> inputList)
+{
+    var level = 0;
+    foreach (var line in inputList)
+    {
+        level = line.Select(x => x == '(' ? 1 : -1).Sum(); 
+    }
+    Console.WriteLine(level);
+}
+
+static void Part02(IList<string> inputList)
+{
+    
+}
+
+var inputList = GetInputList();
+
+Part01(inputList);
+Part02(inputList);
